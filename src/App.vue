6535ade211d424685d14d329083c44de0da3fe2a
craigcliff <craigcliff@gmail.com>
2021-04-10 16:36:48 +0200

    testing more branching

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App - testing build through netlify - test 3"/>
+    <HelloWorld msg="Welcome to Your Vue.js App - testing build through netlify - test 4"/>
   </div>
 </template>
 
